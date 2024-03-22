5579a58aaf8d6727ce6ee3c857650028e1ad97e0
Honahuku <tech@honahuku.com>
2024-03-22 20:54:25 +0900

    fix: 外部から curl するのをやめた

---
+++ image/README.md
@@ -0,0 +1,6 @@
+# image
+```bash
+docker build -t honahuku007/k8s_scale_test:latest .
+docker login
+docker push honahuku007/k8s_scale_test:latest
+```
---
+++ image/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+url="https://raw.githubusercontent.com/honahuku/k8s-hands-on/main/manifest/k8s-scale-test/condition.txt"
+
+while true; do
+	condition=$(curl -s $url)
+	current_time=$(date '+%Y-%m-%d %H:%M:%S')
+
+	if [ "$condition" = "0" ]; then
+		echo "[$current_time] Condition is 0. waiting for 10 seconds..."
+		sleep 10
+	elif [ "$condition" = "1" ]; then
+		echo "[$current_time] Condition is 1. Running stress-ng..."
+		stress-ng --cpu 8 --timeout 10s
+	else
+		echo "Unexpected condition value: $condition"
+	fi
+done
